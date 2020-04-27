de443925e8554dbb6e644faa098680a1a773e63d
ancampo1 <tonycampos19@gmail.com>
2020-04-27 15:13:12 -0500

    implimented check to see if names were prompted

--- Tron/src/application/SecondViewController.java
+++ Tron/src/application/SecondViewController.java
@@ -5,20 +5,26 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SecondViewController implements EventHandler{
 	
 	private Main main;
-	
+	@FXML private TextField player1;
+	@FXML private TextField player2;
 	@Override
 	public void handle(Event event) {
-		MainMenuController game = new MainMenuController();
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		game.start(window);
+		if(!player1.getText().isEmpty() && !player2.getText().isEmpty())
+		{
+			MainMenuController game = new MainMenuController();
+			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+			game.start(window);
+		}
 	}
 }
 
