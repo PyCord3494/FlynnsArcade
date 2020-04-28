7203f99908f37c51d1d0dcc3b70f9eb7aea9b60f
PyCord3494 <wilsonis123@icloud.com>
2020-04-28 18:59:24 -0500

    Fixed scope of variable

--- Tron/src/application/model/Player.java
+++ Tron/src/application/model/Player.java
@@ -13,7 +13,7 @@ import javafx.scene.paint.Color;
 public class Player
 {
 	public ArrayList<Block> blocks1 = new ArrayList<Block>();
-	ArrayList<Block> blocks2 = new ArrayList<Block>();
+	public ArrayList<Block> blocks2 = new ArrayList<Block>();
 
 	public Block head;
 	public Block tail;
